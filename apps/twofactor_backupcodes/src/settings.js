7948df9884fd4ba039329e8657f9869661140fa7
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 16:01:50 -0400

    Initialize bugfix/noid/tooltip-hide-date

---
+++ apps/twofactor_backupcodes/src/settings.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import PersonalSettings from './views/PersonalSettings';
+import store from './store';
+
+Vue.prototype.t = t;
+
+const initialState = OCP.InitialState.loadState('twofactor_backupcodes', 'state');
+store.replaceState(
+	initialState
+)
+
+const View = Vue.extend(PersonalSettings)
+new View({
+	store
+}).$mount('#twofactor-backupcodes-settings')
