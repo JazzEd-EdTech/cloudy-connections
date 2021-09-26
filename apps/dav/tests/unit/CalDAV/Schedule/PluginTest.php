9d0b490324e2116c3719bba168b6c4fcf14a93d4
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 16:12:01 -0400

    Initialize oc-server-private

---
+++ apps/dav/tests/unit/CalDAV/Schedule/PluginTest.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016 Lukas Reschke <lukas@statuscode.ch>
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ * @author Lukas Reschke <lukas@statuscode.ch>
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\DAV\Tests\unit\CalDAV\Schedule;
+
+use OCA\DAV\CalDAV\Schedule\Plugin;
+use Sabre\DAV\Server;
+use Sabre\DAV\Xml\Property\Href;
+use Sabre\VObject\Parameter;
+use Sabre\VObject\Property\ICalendar\CalAddress;
+use Test\TestCase;
+
+class PluginTest extends TestCase  {
+	/** @var Plugin */
+	private $plugin;
+	/** @var Server|\PHPUnit_Framework_MockObject_MockObject */
+	private $server;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->server = $this->createMock(Server::class);
+
+		$this->plugin = new Plugin();
+		$this->plugin->initialize($this->server);
+	}
+
+	public function testGetAddressesForPrincipal() {
+		$href = $this->createMock(Href::class);
+		$href
+			->expects($this->once())
+			->method('getHrefs')
+			->willReturn(['lukas@nextcloud.com', 'rullzer@nextcloud.com']);
+		$this->server
+			->expects($this->once())
+			->method('getProperties')
+			->with(
+				'MyPrincipal',
+				[
+					'{urn:ietf:params:xml:ns:caldav}calendar-user-address-set',
+				]
+			)
+			->willReturn([
+				'{urn:ietf:params:xml:ns:caldav}calendar-user-address-set' => $href
+			]);
+
+		$result = $this->invokePrivate($this->plugin, 'getAddressesForPrincipal', ['MyPrincipal']);
+		$this->assertSame(['lukas@nextcloud.com', 'rullzer@nextcloud.com'], $result);
+	}
+
+
+	public function testGetAddressesForPrincipalEmpty() {
+		$this->server
+			->expects($this->once())
+			->method('getProperties')
+			->with(
+				'MyPrincipal',
+				[
+					'{urn:ietf:params:xml:ns:caldav}calendar-user-address-set',
+				]
+			)
+			->willReturn(null);
+
+		$result = $this->invokePrivate($this->plugin, 'getAddressesForPrincipal', ['MyPrincipal']);
+		$this->assertSame([], $result);
+	}
+
+	public function testStripOffMailTo() {
+		$this->assertEquals('test@example.com', $this->invokePrivate($this->plugin, 'stripOffMailTo', ['test@example.com']));
+		$this->assertEquals('test@example.com', $this->invokePrivate($this->plugin, 'stripOffMailTo', ['mailto:test@example.com']));
+	}
+
+	public function testGetAttendeeRSVP() {
+		$property1 = $this->createMock(CalAddress::class);
+		$parameter1 = $this->createMock(Parameter::class);
+		$property1->expects($this->once())
+			->method('offsetGet')
+			->with('RSVP')
+			->willReturn($parameter1);
+		$parameter1->expects($this->once())
+			->method('getValue')
+			->with()
+			->willReturn('TRUE');
+
+		$property2 = $this->createMock(CalAddress::class);
+		$parameter2 = $this->createMock(Parameter::class);
+		$property2->expects($this->once())
+			->method('offsetGet')
+			->with('RSVP')
+			->willReturn($parameter2);
+		$parameter2->expects($this->once())
+			->method('getValue')
+			->with()
+			->willReturn('FALSE');
+
+		$property3 = $this->createMock(CalAddress::class);
+		$property3->expects($this->once())
+			->method('offsetGet')
+			->with('RSVP')
+			->willReturn(null);
+
+		$this->assertTrue($this->invokePrivate($this->plugin, 'getAttendeeRSVP', [$property1]));
+		$this->assertFalse($this->invokePrivate($this->plugin, 'getAttendeeRSVP', [$property2]));
+		$this->assertFalse($this->invokePrivate($this->plugin, 'getAttendeeRSVP', [$property3]));
+	}
+}
