fd44678ace792a1442e3cb6b61851babb2d2a41f
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 15:45:35 -0400

    Initialize patch/noid/run-avatar-cleanup-later

---
+++ apps/oauth2/l10n/ko.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oauth2",
+    {
+    "Your redirect URL needs to be a full URL for example: https://yourdomain.com/path" : "리다이렉트 URL은 예시와 같이 완전한 URL으로 이루어져야 합니다. 예시: https://yourdomain.com/path",
+    "OAuth 2.0 clients" : "OAuth 2.0 클라이언트",
+    "Name" : "이름",
+    "Redirection URI" : "전환될 URI",
+    "Client Identifier" : "클라이언트 식별자",
+    "Secret" : "비밀 값",
+    "Add client" : "클라이언트 추가",
+    "Add" : "추가"
+},
+"nplurals=1; plural=0;");
