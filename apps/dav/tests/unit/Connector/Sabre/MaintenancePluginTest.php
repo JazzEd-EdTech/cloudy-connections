bf6f4858d857871635aef8d4053d8d8c4167e553
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 15:56:11 -0400

    Initialize smb-filter-forbidden-folders

---
+++ apps/dav/tests/unit/Connector/Sabre/MaintenancePluginTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ * @author Morris Jobke <hey@morrisjobke.de>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\DAV\Tests\unit\Connector\Sabre;
+
+use OCA\DAV\Connector\Sabre\MaintenancePlugin;
+use Test\TestCase;
+use OCP\IConfig;
+
+/**
+ * Class MaintenancePluginTest
+ *
+ * @package OCA\DAV\Tests\unit\Connector\Sabre
+ */
+class MaintenancePluginTest extends TestCase {
+	/** @var IConfig */
+	private $config;
+	/** @var MaintenancePlugin */
+	private $maintenancePlugin;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->config = $this->getMockBuilder(IConfig::class)->getMock();
+		$this->maintenancePlugin = new MaintenancePlugin($this->config);
+	}
+
+	/**
+	 * @expectedException \Sabre\DAV\Exception\ServiceUnavailable
+	 * @expectedExceptionMessage System in maintenance mode.
+	 */
+	public function testMaintenanceMode() {
+		$this->config
+			->expects($this->exactly(1))
+			->method('getSystemValueBool')
+			->with('maintenance')
+			->will($this->returnValue(true));
+
+		$this->maintenancePlugin->checkMaintenanceMode();
+	}
+
+}
