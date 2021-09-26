46725654f61d3ce713e36556c8c168347f96e921
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 15:37:18 -0400

    Initialize bugfix/noid/css-vars-watch-ie

---
+++ apps/federation/l10n/lt_LT.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "federation",
+    {
+    "Added to the list of trusted servers" : "Pridėtas į patikimų serverių sąrašą",
+    "Server is already in the list of trusted servers." : "Serveris jau yra patikimų serverių sąraše.",
+    "Could not add server" : "Nepavyko pridėti serverio",
+    "Federation" : "Išorinė saugykla",
+    "Federation allows you to connect with other trusted servers to exchange the user directory." : "Federacija jums leidžia prisijungti prie kitų patikimų serverių, kad pakeisti vartotojo katalogą.",
+    "Trusted servers" : "Patikimi serveriai",
+    "+ Add trusted server" : "+ Pridėti patikimą serverį",
+    "Trusted server" : "Patikimas serveris",
+    "Add" : "Pridėti"
+},
+"nplurals=4; plural=(n % 10 == 1 && (n % 100 > 19 || n % 100 < 11) ? 0 : (n % 10 >= 2 && n % 10 <=9) && (n % 100 > 19 || n % 100 < 11) ? 1 : n % 1 != 0 ? 2: 3);");
