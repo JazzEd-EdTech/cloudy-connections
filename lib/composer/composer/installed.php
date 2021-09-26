6d3b7fb88b236d680e05d8ac1b07c6125e2764c3
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 19:48:43 -0400

    Initialize enhancement/logger-function

---
+++ lib/composer/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '07278ed57d6997f34be35141ed5fa10dc6ee353a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '07278ed57d6997f34be35141ed5fa10dc6ee353a',
+    ),
+  ),
+);
