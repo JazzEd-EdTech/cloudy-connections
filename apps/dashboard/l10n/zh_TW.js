93627099dd06a01173550454f80e94fc7f58d620
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 18:22:11 -0400

    Initialize enh/noid/displayname-change-activity

---
+++ apps/dashboard/l10n/zh_TW.js
@@ -0,0 +1,28 @@
+OC.L10N.register(
+    "dashboard",
+    {
+    "Dashboard" : "儀表板",
+    "Show something" : "顯示一些東西",
+    "Customize" : "自訂",
+    "Edit widgets" : "編輯小工具",
+    "Get more widgets from the app store" : "從應用商店取得更多小工具",
+    "Change background image" : "變更背景圖片",
+    "Weather service" : "天氣服務",
+    "Weather data from Met.no" : "氣象資訊來自 Met.no",
+    "Weather" : "天氣",
+    "Status" : "狀態",
+    "Good morning" : "早安",
+    "Good morning, {name}" : "早安，{name}",
+    "Good afternoon" : "午安",
+    "Good afternoon, {name}" : "午安，{name}",
+    "Good evening" : "下午好",
+    "Good evening, {name}" : "早安，{name}",
+    "Pick from Files" : "從檔案選取",
+    "Default images" : "預設圖片",
+    "Plain background" : "簡單背景",
+    "Insert from {productName}" : "插入自 {productName}",
+    "Good night, {name}" : "晚安，{name}",
+    "Good night" : "晚安",
+    "Pick from files" : "從檔案選取"
+},
+"nplurals=1; plural=0;");
