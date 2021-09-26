f188ec865df647bc378ae8ef9c16ab9d97c81668
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2021-09-26 16:21:55 -0400

    Initialize fix/19127/deprecated-ldap_control_paged_result

---
+++ apps/dav/lib/CardDAV/UserAddressBooks.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\DAV\CardDAV;
+
+use OCA\DAV\AppInfo\PluginManager;
+use OCA\DAV\CardDAV\Integration\ExternalAddressBook;
+use OCP\IConfig;
+use OCP\IL10N;
+use Sabre\CardDAV\Backend;
+use Sabre\DAV\Exception\MethodNotAllowed;
+use Sabre\DAV\MkCol;
+
+class UserAddressBooks extends \Sabre\CardDAV\AddressBookHome {
+
+	/** @var IL10N */
+	protected $l10n;
+
+	/** @var IConfig */
+	protected $config;
+
+	/** @var PluginManager */
+	private $pluginManager;
+
+	public function __construct(Backend\BackendInterface $carddavBackend,
+								string $principalUri,
+								PluginManager $pluginManager) {
+		parent::__construct($carddavBackend, $principalUri);
+		$this->pluginManager = $pluginManager;
+	}
+
+	/**
+	 * Returns a list of address books
+	 *
+	 * @return array
+	 */
+	function getChildren() {
+		if ($this->l10n === null) {
+			$this->l10n = \OC::$server->getL10N('dav');
+		}
+		if ($this->config === null) {
+			$this->config = \OC::$server->getConfig();
+		}
+
+		$addressBooks = $this->carddavBackend->getAddressBooksForUser($this->principalUri);
+		$objects = array_map(function(array $addressBook) {
+			if ($addressBook['principaluri'] === 'principals/system/system') {
+				return new SystemAddressbook($this->carddavBackend, $addressBook, $this->l10n, $this->config);
+			}
+
+			return new AddressBook($this->carddavBackend, $addressBook, $this->l10n);
+		}, $addressBooks);
+		foreach ($this->pluginManager->getAddressBookPlugins() as $plugin) {
+			$plugin->fetchAllForAddressBookHome($this->principalUri);
+		}
+		return $objects;
+
+	}
+
+	public function createExtendedCollection($name, MkCol $mkCol) {
+		if (ExternalAddressBook::doesViolateReservedName($name)) {
+			throw new MethodNotAllowed('The resource you tried to create has a reserved name');
+		}
+
+		parent::createExtendedCollection($name, $mkCol);
+	}
+
+	/**
+	 * Returns a list of ACE's for this node.
+	 *
+	 * Each ACE has the following properties:
+	 *   * 'privilege', a string such as {DAV:}read or {DAV:}write. These are
+	 *     currently the only supported privileges
+	 *   * 'principal', a url to the principal who owns the node
+	 *   * 'protected' (optional), indicating that this ACE is not allowed to
+	 *      be updated.
+	 *
+	 * @return array
+	 */
+	function getACL() {
+
+		$acl = parent::getACL();
+		if ($this->principalUri === 'principals/system/system') {
+			$acl[] = [
+				'privilege' => '{DAV:}read',
+				'principal' => '{DAV:}authenticated',
+				'protected' => true,
+			];
+		}
+
+		return $acl;
+	}
+
+}
